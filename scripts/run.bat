eeeaeddd6cf11b2e4d19822e127919f1d666a735
Anton Zhivov <drel94@mail.ru>
2021-01-16 03:17:07 +0300

    Initial commit

---
+++ scripts/run.bat
@@ -0,0 +1,4 @@
+@echo off
+cd ..
+cd target
+java -jar DPRParser-1.0-jar-with-dependencies.jar
---
+++ src/main/java/ru/zhivov/Worker.java
@@ -0,0 +1,7 @@
+package ru.zhivov;
+
+public class Worker {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
